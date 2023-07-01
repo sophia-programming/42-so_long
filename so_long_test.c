251410081d6e073b099df26840f6d3385d309450
aoba_okamoto <hacchi0803.rera1126@gmail.com>
2023-07-01 11:04:33 +0900

    test追加

---
+++ so_long_test.c
@@ -0,0 +1,19 @@
+//#include "./so_long/mlx_linux/mlx.h"
+//
+//
+//int main(void)
+//{
+//	void *mlx_ptr;
+//	void *win_ptr;
+//
+//	mlx_ptr = mlx_init();
+//	win_ptr = mlx_new_window(mlx_ptr, 500, 500, "mlx 42");
+//	mlx_loop(mlx_ptr);
+//}
+///*
+// * void *mlx_init(void)　→　自分のソフトウェアとosのディスプレイを接続する関数
+// * void *mlx_new_window(void mlx_ptr, int size_x, int size_y, char *title)
+// *                     　→　ディスプレイに新しいウィンドウを表示する関数。
+// *int mlx_loop(void *mlx_ptr)
+// *                   　　→　表示されたウィンドウでキーボードとマウスの入力を待つ
+// */
